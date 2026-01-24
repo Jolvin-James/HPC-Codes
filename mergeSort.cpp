420cdbd47bb4c5da35f633dfd064b74ecc97de67
Jolvin-James <jolvinjames.zx@gmail.com>
2026-01-24 05:23:16 +0000

    merge sort

---
+++ mergeSort.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int temp[], int left, int mid, int right) {
+    int i = left;
+    int j = mid + 1;
+    int k = left;
+
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while (i <= mid)
+        temp[k++] = arr[i++];
+
+    while (j <= right)
+        temp[k++] = arr[j++];
+
+    for (int x = left; x <= right; x++)
+        arr[x] = temp[x];
+}
+
+void mergeSort(int arr[], int n) {
+    int temp[n];
+
+    for (int size = 1; size < n; size *= 2) {
+        for (int left = 0; left < n - 1; left += 2 * size) {
+            int mid = min(left + size - 1, n - 1);
+            int right = min(left + 2 * size - 1, n - 1);
+
+            merge(arr, temp, left, mid, right);
+        }
+    }
+}
+
+int main() {
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
